245654bef0b3b23974a756d587c31918a30c151f
prem1903 <78896585+prem1903@users.noreply.github.com>
2025-06-11 19:51:00 +0100

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,9 +2,9 @@ output "cluster_name" {
   value = module.eks.cluster_name
 }
 
-output "kubeconfig" {
-  value = module.eks.kubeconfig
-}
+# output "kubeconfig" {
+#  value = module.eks.kubeconfig
+# }
 
 output "cluster_endpoint" {
   value = module.eks.cluster_endpoint
